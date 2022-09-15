e4816bebbec6ba0efd96db5251643cde14bbaefc
Abel Colina <abdv.vargas@gmail.com>
2022-09-15 01:21:38 -0400

    fusionado mobile menu

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const email = document.querySelector('.navbar-email');
 const menu = document.querySelector('.desktop-menu');
+const burgericon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 function toogleMenu(){
     menu.classList.toggle('inactive');
 }
 
+function toogleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
 
 email.addEventListener('click', toogleMenu);
+burgericon.addEventListener('click', toogleMobileMenu);
 
